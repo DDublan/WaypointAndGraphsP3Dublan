8704eed201449d356a17296cfde45be6a6ee7624
Dereck Dublan <750284@student.dusd.net>
2025-01-08 11:45:43 -0800

    bower

--- WaypointAndGraphsP3Dublan/Assets/FollowWP.cs
+++ WaypointAndGraphsP3Dublan/Assets/FollowWP.cs
@@ -8,7 +8,9 @@ public class FollowWP : MonoBehaviour
     public GameObject[] waypoints;
     int currentWP = 0;
 
+
     public float speed = 10.0f;
+    public float rotSpeed = 10.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +26,11 @@ public class FollowWP : MonoBehaviour
         if (currentWP >= waypoints.Length)
             currentWP = 0;
 
-        Quaternion lookatWP = Quaterinion.LookRotation(waypoints[currentWP])
-    }
+        Quaternion lookatWP = Quaternion.LookRotation(waypoints[currentWP].transform.position - this.transform.position);
+
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookatWP, rotSpeed * Time.deltaTime);
+    
+        this.transform.Translate(0, 0, speed * Time.deltaTime);
+    
+    }   
 }
